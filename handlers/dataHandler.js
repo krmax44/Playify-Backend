44046197609e7eb0b43c6e5df26919f8285a2599
krmax44 <hi@krmax44.de>
2019-08-29 22:10:00 +0200

    refactor

--- handlers/dataHandler.js
+++ handlers/dataHandler.js
@@ -1,49 +1,38 @@
-const Obj = obj => Object.assign({}, obj);
-
 const artist = $artist => $artist.name;
 
 const artists = $artists => $artists.map($artist => artist($artist));
 
-const album = $album =>
-	Obj({
-		name: $album.name,
-		images: $album.images
-	});
+const album = $album => ({
+	name: $album.name,
+	images: $album.images
+});
 
 const albums = $albums =>
-	$albums.map($album =>
-		Obj({
-			name: $album.name,
-			images: $album.images,
-			id: $album.id
-		})
-	);
+	$albums.map($album => ({
+		name: $album.name,
+		images: $album.images,
+		id: $album.id
+	}));
 
 const albumTracks = $tracks =>
-	$tracks.map($track =>
-		Obj({
-			artists: artists($track.artists),
-			name: $track.name
-		})
-	);
+	$tracks.map($track => ({
+		artists: artists($track.artists),
+		name: $track.name
+	}));
 
 const artistTracks = $tracks =>
-	$tracks.map($track =>
-		Obj({
-			artists: artists($track.artists),
-			name: $track.name,
-			album: album($track.album)
-		})
-	);
+	$tracks.map($track => ({
+		artists: artists($track.artists),
+		name: $track.name,
+		album: album($track.album)
+	}));
 
 const playlistTracks = $tracks =>
-	$tracks.map($track =>
-		Obj({
-			artists: artists($track.track.artists),
-			name: $track.track.name,
-			album: album($track.track.album)
-		})
-	);
+	$tracks.map($track => ({
+		artists: artists($track.track.artists),
+		name: $track.track.name,
+		album: album($track.track.album)
+	}));
 
 module.exports = {
 	artist,
